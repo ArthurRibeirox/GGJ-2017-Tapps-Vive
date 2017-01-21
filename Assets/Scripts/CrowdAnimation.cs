40b3a8c9b283f39c1e9078b204c2beed26578697
VR <VR>
2017-01-21 03:22:34 -0300

    Added a person animation

---
+++ Assets/Scripts/CrowdAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdAnimation : MonoBehaviour {
+	string[] animations = { "applause", "applause2", "celebration", "celebration2", "celebration3" }; 
+
+    
+	void Start () {
+		gameObject.GetComponent<Animation>()["idle"].wrapMode = WrapMode.Loop;
+	}
+	
+
+    private void OnTriggerEnter(Collider other)
+    {
+        int index = Random.Range(0, animations.Length);
+        gameObject.GetComponent<Animation>().Play(animations[index]);
+        gameObject.GetComponent<Animation>().PlayQueued("idle");
+    }
+}
--- Assets/Scripts/CrowdGenerator.cs
+++ Assets/Scripts/CrowdGenerator.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class CrowdGenerator : MonoBehaviour {
     public float density = 1;
+    public float variance = 0.1f;
     public GameObject person;
     public GameObject crowdStart;
     public GameObject crowdEnd;
+    public float radius = 9;
 
 
     // Use this for initialization
     void Start () {
         int b = 0;
 
-        for (float i = crowdStart.transform.position.x; i < crowdEnd.transform.position.x; i += density)
+        for (float i = crowdStart.transform.position.x; i < crowdEnd.transform.position.x; i += density) {
             for (float j = crowdStart.transform.position.z; j < crowdEnd.transform.position.z; j += density)
             {
-                Instantiate(person, new Vector3(i, 0.67f, j), Quaternion.identity);
+                if (i*i + j*j <= radius) continue;
+                
+                Vector3 position = new Vector3(i + Random.Range(-variance, variance), -1.67f, j + Random.Range(-variance, variance));
+
+                Instantiate(
+                    person,
+                    position,
+                    Quaternion.LookRotation(new Vector3(-position.x, 0, -position.z))
+                );
                 b += 1;
             }
+        }
 
         print(b);
 	}
