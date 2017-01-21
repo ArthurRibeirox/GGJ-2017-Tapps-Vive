e84df4cc265766544306e3d8414196ee7630dfc6
VR <VR>
2017-01-21 05:32:03 -0300

    Integrated sounds.

---
+++ Assets/Scripts/PickStrokes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickStrokes : MonoBehaviour {
+	// public GameObject strings;
+	// public GameObject controller;
+	// public float maxDistanceStrings;
+	// public float maxHeightStrings;
+
+	// private bool onUpSide = false;
+	
+	// // Update is called once per frame
+	// void Update () {
+	// 	Vector3 pickPosition = strings.transform.InverseTransformPoint(controller.transform.TransformPoint(controller.transform.position));
+	// 	float distance = pickPosition.x*pickPosition.x + pickPosition.y*pickPosition.y;
+	// 	float distanceZ = pickPosition.z;
+
+	// 	if(distance > maxDistanceStrings || (distanceZ > maxHeightStrings && distanceZ < -0.1)) return;
+
+	// 	if (onUpSide && pickPosition.y < 0) {
+	// 		print("stroke");
+	// 		onUpSide = false;
+	// 	}
+	// 	else if (!onUpSide && pickPosition.y > 0) {
+	// 		print("stroke");
+	// 		onUpSide = true;
+	// 	}
+	// }
+
+	public PlaySong playSongScript;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+    	playSongScript.playGuitar();
+    }
+}
