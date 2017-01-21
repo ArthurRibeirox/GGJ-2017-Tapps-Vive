2723f27cfdc27524ec74b13de234528b8121bd10
fabiogunkel-tapps <fabio.gunkel@tapps.com.br>
2017-01-21 14:56:00 -0200

    added some scripts

--- Assets/Scripts/CrowdAnimation.cs
+++ Assets/Scripts/CrowdAnimation.cs
@@ -3,18 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CrowdAnimation : MonoBehaviour {
+    public bool bored;
+    public float time;
+    public int score;
 	string[] animations = { "applause", "applause2", "celebration", "celebration2", "celebration3" }; 
 
     
 	void Start () {
+        score = 0;
+        bored = false;
+        time = 0;
 		gameObject.GetComponent<Animation>()["idle"].wrapMode = WrapMode.Loop;
 	}
 	
 
     private void OnTriggerEnter(Collider other)
     {
+        if(bored == true)
+        {
+            score += 50;
+            print(score);
+        }
+        bored = false;
         int index = Random.Range(0, animations.Length);
         gameObject.GetComponent<Animation>().Play(animations[index]);
         gameObject.GetComponent<Animation>().PlayQueued("idle");
+    }
+
+    public void startBoring(){
+        bored = true;
+        print("ficou entediado");
+
+    }
+
+    void Update(){
+        time += Time.deltaTime;
+        if(bored == true && animations.Length < time){
+        print("animou o tedio");
+        int index = Random.Range(0, animations.Length);
+        gameObject.GetComponent<Animation>().Play(animations[index]);
+        gameObject.GetComponent<Animation>().PlayQueued("idle");
+        time = 0;
+        }
     }
 }
--- Assets/Scripts/CrowdGenerator.cs
+++ Assets/Scripts/CrowdGenerator.cs
@@ -9,11 +9,18 @@ public class CrowdGenerator : MonoBehaviour {
     public GameObject crowdStart;
     public GameObject crowdEnd;
     public float radius = 9;
-
+    static public List<GameObject> crowd;
+    public CrowdAnimation crowdAnimation;
+    public GameObject personObject;
+    static public bool ended;
+    public float time;
 
     // Use this for initialization
     void Start () {
+        ended = false;
+        crowd = new List<GameObject>();
         int b = 0;
+        time = 0;
 
         for (float i = crowdStart.transform.position.x; i < crowdEnd.transform.position.x; i += density) {
             for (float j = crowdStart.transform.position.z; j < crowdEnd.transform.position.z; j += density)
@@ -22,20 +29,41 @@ public class CrowdGenerator : MonoBehaviour {
                 
                 Vector3 position = new Vector3(i + Random.Range(-variance, variance), -1.67f, j + Random.Range(-variance, variance));
 
-                Instantiate(
+                personObject = Instantiate(
                     person,
                     position,
                     Quaternion.LookRotation(new Vector3(-position.x, 0, -position.z))
                 );
+                crowd.Add(personObject);
                 b += 1;
             }
         }
 
         print(b);
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ended) { 
+            time += Time.deltaTime;
+            if (time > 0.1f)
+            {
+                print("ficou entediado");
+                int randomNumber = Random.Range(0, crowd.Count - 1);
+                print(randomNumber);
+                crowd[randomNumber].GetComponent<CrowdAnimation>().startBoring();
+                time -= 2;
+            }
+        }
 	}
+
+    public static void endGame()
+    {
+        ended = true;
+        foreach(GameObject person in crowd){
+            Object.Destroy(person);
+        }
+    }
 }
